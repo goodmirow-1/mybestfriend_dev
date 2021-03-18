41395731cb9badac6c4def35259ea57ed09ade17
roy <goodmirow@gmail.com>
2021-03-18 17:51:45 +0900

    ajkqkw

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const methodOverride = require('method-override');		//Post,Delete,Update 관련 
 const bodyParser = require('body-parser');			//Json으로 데이터 통신 Module
 const helmet = require('helmet');				//http 보안관련 Module
 
+const models = require("./models/index.js");
+
 const app = express();
 
 app.use(methodOverride('_method'));
@@ -34,7 +36,7 @@ app.post('/', function(req, res) {
         return;
     }
 
-    console.log(req.body)
+    console.log(req.body);
     res.status(200).send('send me ping');
 })
 
@@ -52,14 +54,14 @@ process.on('SIGINT', shutDown); //비정상종료
 
 function shutDown() {
 
-	logger.info('Received kill signal, shutting down gracefully');
+	console.log('Received kill signal, shutting down gracefully');
 	server.close(() => {
-		logger.info('Closed out rmaining connections');
+		console.log('Closed out rmaining connections');
 		process.exit(0);
 	});
 
 	setTimeout(() => {
-		logger.error('Could not close connections in time, forcefully shutting down');
+		console.log('Could not close connections in time, forcefully shutting down');
 		process.exit(1);
 	}, 10000);
 }
