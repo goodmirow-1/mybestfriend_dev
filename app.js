76cd9fb8d8fa1f4b513a248a794211fd91a8fd24
roy <goodmirow@gmail.com>
2021-03-18 17:50:34 +0900

    ajkqkw

--- app.js
+++ app.js
@@ -2,3 +2,64 @@ const express = require('express');
 const methodOverride = require('method-override');		//Post,Delete,Update 관련 Module
 const bodyParser = require('body-parser');			//Json으로 데이터 통신 Module
 const helmet = require('helmet');				//http 보안관련 Module
+
+const app = express();
+
+app.use(methodOverride('_method'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname));
+app.use(helmet());
+
+app.set('port', process.argv[2] || process.env.PORT || 50000);
+const server = app.listen(app.get('port'), () => {
+	console.log('Express server listening on port ' + app.get('port'));
+
+});
+
+// sequelize 연동
+models.sequelize.sync().then( () => {
+	console.log("DB Connect Success");
+}).catch( err => {
+    console.log("DB Connect Faield");
+    console.log(err);
+})
+
+app.post('/', function(req, res) {
+    const {data} = req.body;
+    if(data === 'ping'){
+        console.log(req.body);
+        res.status(200).send('pong');
+        return;
+    }
+
+    console.log(req.body)
+    res.status(200).send('send me ping');
+})
+
+app.use(function(req,res,next){
+	if(isDisableKeepAlive){
+		res.set('Connection', 'close');
+	}
+	next();
+});
+
+let isDisableKeepAlive = false;
+
+process.on('SIGTERM', shutDown); //정상종료 
+process.on('SIGINT', shutDown); //비정상종료
+
+function shutDown() {
+
+	logger.info('Received kill signal, shutting down gracefully');
+	server.close(() => {
+		logger.info('Closed out rmaining connections');
+		process.exit(0);
+	});
+
+	setTimeout(() => {
+		logger.error('Could not close connections in time, forcefully shutting down');
+		process.exit(1);
+	}, 10000);
+}
