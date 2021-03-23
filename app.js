9526921cf61f137dfd98cd518ab6ffa751f2eb91
roy <goodmirow@gmail.com>
2021-03-23 17:14:04 +0900

    FILE_TEST

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ function shutDown() {
 	}, 10000);
 }
 
-app.get('/TEST', function(req, res, next) {
+app.get('/FILE_TEST', function(req, res, next) {
 	var upload_folder = '업로드 폴더 경로';
 	//var file = upload_folder + req.body.file_name; // ex) /upload/files/sample.txt
 	
@@ -78,7 +78,7 @@ app.get('/TEST', function(req, res, next) {
 	try {
 	  if (fs.existsSync(file)) { // 파일이 존재하는지 체크
 		var filename = path.basename(file); // 파일 경로에서 파일명(확장자포함)만 추출
-		var mimetype = mime.getType(file); // 파일의 타입(형식)을 가져옴
+		var mimetype = mime.lookup(file); // 파일의 타입(형식)을 가져옴
 	  
 		res.setHeader('Content-disposition', 'attachment; filename=' + filename); // 다운받아질 파일명 설정
 		res.setHeader('Content-type', mimetype); // 파일 형식 지정
