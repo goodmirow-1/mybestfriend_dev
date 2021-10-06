70ed45d53ceb5a8b26dd706658d4277bd05748cb
khjsla <rkdguswlsla@gmail.com>
2021-10-06 11:23:39 +0900

    check

--- config/configure.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-    secret: "abcdemysecrettoken",
-    refreshsecret: "fghijmyrefereshsecrettoken",
-    pwdsecret: "mysecretpassword",
-    "port": 50000,
-    "tokenLife": 60 * 60, 
-    "refreshTokenLife": 30 * 60 * 60 * 60
-}
